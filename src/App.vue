c5be5390bd117880946397ee3167dc01e8a8eba5
sunbaoning <sunbaoning8@126.com>
2019-12-19 12:53:14 +0800

    jsonp报错问题解决，方法是删除node_modles文件重新下载

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        {{datalist}}
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import jsonp from 'jsonp'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  mounted () {
-      // let url = 'https://new-api.meiqia.com/visit/get_base_config';
-        // jsonp(url, () => {
-
-        // })
-  }
+    name: 'app',
+    data() {
+        return {
+            datalist: {}
+        }
+    },
+    components: {
+        HelloWorld
+    },
+    mounted() {
+        let url = 'https://www.imooc.com/search/hotwords';
+        jsonp(url, (err,src) => {
+            this.datalist = src
+        })
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
